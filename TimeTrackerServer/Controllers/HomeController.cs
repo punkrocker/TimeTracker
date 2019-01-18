aacd5d863ffb641af8fe5d9e6dd261304810658a
punkrocker <punkrock_2000@163.com>
2019-01-18 23:13:48 +0800

    添加登录验证属性

--- TimeTrackerServer/Controllers/HomeController.cs
+++ TimeTrackerServer/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TimeTrackerServer.App_Start;
 
 namespace TimeTrackerServer.Controllers
 {
+    [Login]
     public class HomeController : Controller
     {
         // GET: Home

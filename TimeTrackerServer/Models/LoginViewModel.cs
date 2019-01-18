8cc079da6cdd0141d54cac540415aeb435959d66
punkrocker <punkrock_2000@163.com>
2019-01-18 22:58:58 +0800

    添加登录model

---
+++ TimeTrackerServer/Models/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+
+namespace TimeTrackerServer.Models
+{
+    public class LoginViewModel : IUser<Guid>
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName
+        {
+            get; set;
+        }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password
+        {
+            get; set;
+        }
+
+        public Guid Id
+        {
+            get
+            {
+                return new Guid();
+            }
+        }
+    }
+}

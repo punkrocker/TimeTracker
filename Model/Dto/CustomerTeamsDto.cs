1c24bb7cfdef62720125a277a244529331a49419
punkrocker <punkrock_2000@163.com>
2019-01-30 00:18:14 +0800

    扩展客户信息类，增加联系人

--- Model/Dto/CustomerTeamsDto.cs
+++ Model/Dto/CustomerTeamsDto.cs
@@ -8,6 +8,7 @@ namespace Model.Dto
     public class CustomerTeamsDto
     {
         public string CustomerName { get; set; }
+        public string Contact { get; set; }
         public int TeamId { get; set; }
         public string TeamName { get; set; }
     }

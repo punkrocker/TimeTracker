11e5f9d04d3f38415a9167501a8de550ed3ec409
punkrocker <punkrock_2000@163.com>
2019-02-12 22:27:09 +0800

    报告页图表合并展示

--- TimeTrackerServer/Controllers/ReportController.cs
+++ TimeTrackerServer/Controllers/ReportController.cs
@@ -26,6 +26,23 @@ namespace TimeTrackerServer.Controllers
         [HttpPost]
         public ActionResult Report(List<ReportDisplayInfo> reportList, string comment, int customerId, string currentMonth)
         {
+            List<ReportDisplayInfo> groupedReportList = new List<ReportDisplayInfo>();
+            reportList.ForEach(c =>
+            {
+                var currentReport = groupedReportList.Where(a => a.ProjectName.Equals(c.ProjectName)).FirstOrDefault();
+                if (currentReport == null)
+                    groupedReportList.Add(c);
+                else
+                {
+                    currentReport.CurrentModify += c.CurrentModify;
+                    currentReport.CurrentTask += c.CurrentTask;
+                    currentReport.CurrentTime += c.CurrentTime;
+                    currentReport.LastModify += c.LastModify;
+                    currentReport.LastTask += c.LastTask;
+                    currentReport.LastTime += c.LastTime;
+                }
+            });
+
             List<String> projectNames = new List<string>();
             List<Int32> lastTasks = new List<Int32>();
             List<Int32> currentTasks = new List<Int32>();
@@ -34,7 +51,7 @@ namespace TimeTrackerServer.Controllers
             List<CustomerTeamsDto> customerTeams = CustomerService.GetCustomerTeams(customerId).ToList();
             List<CustomerProjectDetail> customerProjectDetails = ReportService.GetCustomerProjectDetails(customerId, currentMonth, Convert.ToInt16(Session["UserId"])).ToList();
             List<List<string>> displayTableInfo = ReportService.GetDisplayCustomerProjectDetails(customerProjectDetails);
-            foreach (ReportDisplayInfo reportDisplayInfo in reportList)
+            foreach (ReportDisplayInfo reportDisplayInfo in groupedReportList)
             {
                 projectNames.Add(reportDisplayInfo.ProjectName);
                 lastTasks.Add(reportDisplayInfo.LastTask);

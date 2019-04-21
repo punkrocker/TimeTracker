ca80711bebf1f525b59cde1a241c214d50db7ef2
punkrocker <punkrock_2000@163.com>
2019-04-21 21:28:02 +0800

    sql翻译为linq

---
+++ TimeTrackerServer/Models/ProjectSubmitInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeTrackerServer.Models
+{
+    public class ProjectSubmitInfo
+    {
+        public int ProjectID { get; set; }
+        public string ProjectName { get; set; }
+        public int UserID { get; set; }
+        public string UseName { get; set; }
+        public int Charge { get; set; }
+        public int Task { get; set; }
+        public int Time { get; set; }
+    }
+}
--- TimeTrackerServer/Services/ProjectService.cs
+++ TimeTrackerServer/Services/ProjectService.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Model;
+using TimeTrackerServer.Models;
 
 namespace TimeTrackerServer.Services
 {
@@ -14,6 +16,7 @@ namespace TimeTrackerServer.Services
         //t2.Charge,
         //isnull(t4.task, 0) as Task,
         //isnull(t4.time, 0) as Time
+
         //    from T_PM_Project t1
         //left join T_PM_Member t2 on t1.ProjectID = t2.ProjectID
         //    left join T_Sys_UserInfo t3 on t2.UserID = t3.UserID
@@ -21,6 +24,41 @@ namespace TimeTrackerServer.Services
         //    from T_PM_Task
         //    where TaskDate = CONVERT(varchar(10), GETDATE(), 120)
         //group by UserID) t4 on t3.UserID = t4.UserID
+
         //    where t1.ProjectID in (select ProjectID from T_PM_Member where UserID = 32 and Charge = 1);
+        public IList<ProjectSubmitInfo> GetProjectSubmitInfos(int userId)
+        {
+            using (var db = new TimeTrackerEntities())
+            {
+                var taskSummary = (from task in db.T_PM_Task
+                        .Where(a => a.TaskDate.Year == DateTime.Today.Year
+                                    && a.TaskDate.Month == DateTime.Today.Month
+                                    && a.TaskDate.Day == DateTime.Today.Day)
+                        .GroupBy(a => new
+                        {
+                            a.UserID
+                        })
+                    select new T_PM_Task
+                    {
+                        UserID = task.Key.UserID,
+                        RealTask = task.Sum(a => a.RealTask),
+                        ReportTime = task.Sum(a => a.ReportTime)
+                    });
+                var projectSubmitInfos = (from project in db.T_PM_Project
+                    join member in db.T_PM_Member on project.ProjectID equals member.ProjectID
+                    join user in db.T_Sys_UserInfo on member.UserID equals user.UserID
+                    join task in taskSummary on member.UserID equals task.UserID
+                    select new ProjectSubmitInfo
+                    {
+                        ProjectID = project.ProjectID,
+                        ProjectName = project.ProjectName,
+                        UserID = user.UserID,
+                        UseName = user.UseName,
+                        Task = Convert.ToInt32(task.RealTask),
+                        Time = Convert.ToInt32(task.ReportTime)
+                    }).ToList();
+                return projectSubmitInfos;
+            }
+        }
     }
 }
